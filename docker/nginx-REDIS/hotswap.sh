41a82ae6006bde033d4611c7ad8f3697c717d0f2
Martin Duong <mdduong@ucdavis.edu>
2019-03-13 21:47:04 +0000

    Modified hotswap.sh

--- docker/nginx-REDIS/hotswap.sh
+++ docker/nginx-REDIS/hotswap.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-set -e
 
-if [ -z $5 ]; then
+if [ -n $5 ]; then
     export REDIS_HOST=$5
 fi
 
 sed -i'' "s/%{REDIS_HOST}/${REDIS_HOST}/" /etc/nginx/nginx.conf
 
-exec "$@"
+eval $(/usr/sbin/nginx -s reload)
